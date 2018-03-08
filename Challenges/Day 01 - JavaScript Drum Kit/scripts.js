b928fa7782c1a911d948fa0f00cb93df8d4b5e93
Uboat46 <uboat46@hotmail.com>
2018-03-08 09:30:16 -0600

    Finished

--- Challenges/Day 01 - JavaScript Drum Kit/scripts.js
+++ Challenges/Day 01 - JavaScript Drum Kit/scripts.js
@@ -1,11 +1,19 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) return; //There is no key binding
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   key.classList.add('playing');
-  if(!audio) return; //There is no key binding
   audio.currentTime = 0;
   audio.play();
 }
+
+function removeTransition(e){
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', function(e){
   e.preventDefault();
