12db1f91b79d50c8f43b7c5b22d9738e2e1e406f
Uboat46 <uboat46@hotmail.com>
2018-03-09 13:35:12 -0600

    added logic and css

---
+++ Challenges/Day 02 - E02 JS and CSS Clock/scripts.js
@@ -0,0 +1,18 @@
+function setTime() {
+  var date = new Date();
+  var secondsDeg = ((date.getSeconds()) * 6) + 90;
+  var minutesDeg = ((date.getMinutes()) * 6) + 90;
+  var hoursDeg = ((date.getHours() % 12) * 15) + 90;
+
+  document.querySelector('.second').style.transform = `rotate(${secondsDeg}deg)`;
+  document.querySelector('.minute').style.transform = `rotate(${minutesDeg}deg)`;
+  document.querySelector('.hour').style.transform = `rotate(${hoursDeg}deg)`;
+}
+
+function init() {
+  setTime();
+  setInterval(setTime, 1000);
+}
+
+
+window.onload = init;
