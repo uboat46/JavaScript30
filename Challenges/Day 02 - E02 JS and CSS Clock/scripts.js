8cd2b3d4b5a8099eaa5cd010b68216bf7874d313
Uboat46 <uboat46@hotmail.com>
2018-03-09 14:18:49 -0600

    Added numbers to clock

--- Challenges/Day 02 - E02 JS and CSS Clock/scripts.js
+++ Challenges/Day 02 - E02 JS and CSS Clock/scripts.js
@@ -2,7 +2,7 @@ function setTime() {
   var date = new Date();
   var secondsDeg = ((date.getSeconds()) * 6) + 90;
   var minutesDeg = ((date.getMinutes()) * 6) + 90;
-  var hoursDeg = ((date.getHours() % 12) * 15) + 90;
+  var hoursDeg = ((date.getHours() % 12) * 30) + 90;
 
   document.querySelector('.second').style.transform = `rotate(${secondsDeg}deg)`;
   document.querySelector('.minute').style.transform = `rotate(${minutesDeg}deg)`;
