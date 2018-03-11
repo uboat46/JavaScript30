e7071b89c9c0147e7ba703ede3728d4b9aecd61c
Uboat46 <uboat46@hotmail.com>
2018-03-10 22:11:06 -0600

    Finished adding logic

--- Challenges/Day 03 - E03 CSS Variables/scripts.js
+++ Challenges/Day 03 - E03 CSS Variables/scripts.js
@@ -1,6 +1,13 @@
+function handleInput() {
+  const metric = this.dataset.metric || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + metric);
+}
+
 function init() {
   const inputs = document.querySelectorAll('input');
   
+  inputs.forEach(input => input.addEventListener('change', handleInput));
+  inputs.forEach(input => input.addEventListener('mousemove', handleInput));  
 }
 
 window.onload = init();
